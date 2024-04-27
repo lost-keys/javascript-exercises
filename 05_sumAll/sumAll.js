0dbc70b3593e20abb4d4ef63e2170f1a5f7f621b
robin <robin.e.unger@gmail.com>
2024-04-27 15:50:44 -0500

    Complete exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  if (num1 < 0 || num2 < 0 ||
+      typeof(num1) != "number" || typeof(num2) != "number") {
+    return "ERROR";
+  }
 
+  start = Math.min(num1, num2);
+  end = Math.max(num1, num2);
+  sum = 0;
+
+  for (i = start; i <= end; i++) {
+    sum += i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
