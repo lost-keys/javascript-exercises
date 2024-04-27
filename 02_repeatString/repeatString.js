3fe4827e6f6ebf55c01166950527e9c53e50ec54
robin <robin.e.unger@gmail.com>
2024-04-27 14:41:18 -0500

    Complete exercise 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function(string, timesToRepeat) {
+  if (timesToRepeat < 0) return("ERROR");
+  result = "";
+  for(i = 0; i < timesToRepeat; i++) {
+    result += string
+  }
 
+  return result;
 };
 
 // Do not edit below this line
