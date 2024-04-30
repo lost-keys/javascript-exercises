9eb6876a59693635ec5988faa3fc787a8e487c80
robin <robin.e.unger@gmail.com>
2024-04-30 17:03:26 -0500

    Complete exercise 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInFahrenheit) {
+  return Math.round( ((tempInFahrenheit - 32) * (5 / 9)) * 10 ) / 10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInCelcius) {
+  return Math.round( (tempInCelcius * (9 / 5) + 32) * 10 ) / 10
 };
 
 // Do not edit below this line
