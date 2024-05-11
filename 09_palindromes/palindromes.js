c10123ee7cdb0bda0e8f473f7ed31ca2a0658309
robin <robin.e.unger@gmail.com>
2024-05-11 16:12:17 -0500

    Complete exercise 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,24 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+  word = cleanString(word);
+  if (reverseString(word) === word) {
+    return true;
+  } else {
+    return false;
+  }
 };
+
+// remove punctuation so that it does not cause the palindrome
+// test to fail
+function cleanString(str) {
+  return str.split("")
+            .filter((char) => /^[a-zA-Z0-9]+$/.test(char))
+            .join("")
+            .toLowerCase();
+}
+
+function reverseString(str) {
+  return str.split("").reverse().join("")
+}
 
 // Do not edit below this line
 module.exports = palindromes;
