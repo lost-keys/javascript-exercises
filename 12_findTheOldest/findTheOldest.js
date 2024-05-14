75230e108036e1501bce446a87a0a2d7f9d1a4d0
robin <robin.e.unger@gmail.com>
2024-05-14 13:06:41 -0500

    Complete exercise 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,21 @@
-const findTheOldest = function() {
+const getAge = function(person) {
+  if ("yearOfDeath" in person) {
+    return person.yearOfDeath - person.yearOfBirth;
+  }
+    date = new Date();
+    currentYear = date.getFullYear();
+    return currentYear - person.yearOfBirth;
+}
 
+const findTheOldest = function(people) {
+  return people.reduce((oldest, current) => {
+    const currentAge = getAge(current);
+    const oldestAge = getAge(oldest);
+    
+    return currentAge > oldestAge ? current : oldest;
+  });
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
