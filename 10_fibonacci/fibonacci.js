45bbfba85c84d0b8a10fdf9a24ff6ac1c14d1c9d
robin <robin.e.unger@gmail.com>
2024-05-14 11:08:53 -0500

    Complete exercise 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(position) {
+  if (position < 0) { 
+    return "OOPS"; 
+  } else if (position == 1) {
+    return 1;
+  }
 
+  let last = 0, next = 1, fib = 0;
+  for (let i = 0; i < position - 1; i++) {
+    if (fib != 0) { last = next; next = fib; }
+    fib = last + next;
+    console.log(fib);
+  }
+
+  return fib;
 };
 
 // Do not edit below this line
