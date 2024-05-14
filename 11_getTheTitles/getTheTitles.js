93cad4c74662e128598ec9f6f5fa20feb0392ead
robin <robin.e.unger@gmail.com>
2024-05-14 11:26:09 -0500

    Complete exercise 11_getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,15 @@
-const getTheTitles = function() {
+// SET titles to empty array
+// FOR each book in books
+//  PUSH book.title to titles
+// END FOR
 
+const getTheTitles = function(books) {
+  // let titles = [];
+  // books.forEach(book => titles.push(book.title));
+  // return titles;
+
+  // Simpler version using Array map()
+  return books.map(book => book.title)
 };
 
 // Do not edit below this line
